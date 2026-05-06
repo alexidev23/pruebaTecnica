5bb11ab47c3b4d5da25b2ca6c015aa40202bbfbd
Alexis <alexisescobardev@gmail.com>
2026-05-06 10:44:45 -0300

    chore: init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 font-extrabold">
+        HOLA MUNDO ESTA ES MI PRIMERA PRUEBA TECNICA
+      </h1>
+    </>
+  );
+}
+
+export default App;
