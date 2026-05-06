8804ad94828525c3d7741a8ebf6fee684460ce06
Alexis <alexisescobardev@gmail.com>
2026-05-06 16:25:32 -0300

    feat: integrate simulator logic and connect components

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { useState } from "react";
 import Header from "./components/Header";
 import HistoryDrawer from "./components/HistoryDrawer";
 import SimulatorForm from "./components/SimulatorForm";
+import SimulatorResults from "./components/SimulatorResults";
+import { useSimulator } from "./hooks/useSimulator";
 
 function App() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const { results, isLoading, simulate } = useSimulator();
 
   return (
     <>
       <Header />
 
-      <main className="px-20 py-10">
+      <main className="px-2.5 xl:px-20 py-10">
         <div className="space-y-1.5">
           <h5 className="text-primary uppercase text-xs">
             Herramienta de Analisis
@@ -22,7 +25,21 @@ function App() {
           </p>
         </div>
 
-        <SimulatorForm onOpenHistory={() => setIsDrawerOpen(true)} />
+        <div className="grid grid-cols-1 gap-8 xl:grid-cols-2 py-12">
+          <SimulatorForm
+            onOpenHistory={() => setIsDrawerOpen(true)}
+            onSubmit={simulate}
+            isLoading={isLoading}
+          />
+          <SimulatorResults
+            capital_invertido={results?.capital_invertido || 0}
+            plazo_en_dias={results?.plazo_en_dias || 0}
+            tna={results?.tna || 0}
+            interes_ganado={results?.interes_ganado || 0}
+            tasa_efectiva_periodo={results?.tasa_efectiva_periodo || 0}
+            showResults={!!results}
+          />
+        </div>
         <HistoryDrawer
           isOpen={isDrawerOpen}
           onClose={() => setIsDrawerOpen(false)}
