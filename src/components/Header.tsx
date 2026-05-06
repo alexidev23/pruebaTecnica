e7635b80a4dfb7a3f85bf5352cecd963fa60206c
Alexis <alexisescobardev@gmail.com>
2026-05-06 16:23:58 -0300

    styles: add styles for view mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { ChartColumn } from "lucide-react";
 
 export default function Header() {
   return (
-    <header className="text-white py-4 px-20 flex justify-between items-center border-b-2 border-muted-foreground/10">
+    <header className="text-white py-4 px-2.5 xl:px-20 flex justify-between items-center border-b-2 border-muted-foreground/10">
       <div className="flex items-center gap-2.5">
         <div className="bg-primary p-2 rounded-md flex items-center justify-center gap-2">
           <ChartColumn size={15} />
