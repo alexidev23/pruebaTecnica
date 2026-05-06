78a2d1b044ed42bc74b2994a7eb838ccedcf79ce
Alexis <alexisescobardev@gmail.com>
2026-05-06 13:29:07 -0300

    feat: integrate layout and base components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import HistoryDrawer from "./components/HistoryDrawer";
+import SimulatorForm from "./components/SimulatorForm";
+
 function App() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
   return (
     <>
-      <h1 className="text-red-500 font-extrabold">
-        HOLA MUNDO ESTA ES MI PRIMERA PRUEBA TECNICA
-      </h1>
+      <Header />
+
+      <main className="px-20 py-10">
+        <div className="space-y-1.5">
+          <h5 className="text-primary uppercase text-xs">
+            Herramienta de Analisis
+          </h5>
+          <h1 className="text-4xl font-bold">Simulador de Plazo Fijo</h1>
+          <p className="text-secondary-foreground max-w-125">
+            Calculá el rendimiento estimado de tu inversión a tasa nominal anual
+            en pesos argentinos.
+          </p>
+        </div>
+
+        <SimulatorForm onOpenHistory={() => setIsDrawerOpen(true)} />
+        <HistoryDrawer
+          isOpen={isDrawerOpen}
+          onClose={() => setIsDrawerOpen(false)}
+        />
+      </main>
     </>
   );
 }
